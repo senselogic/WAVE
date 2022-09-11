e0be53f4e51b61df9f18873c97755b12eabc1274
senselogic <dev.senselogic@gmail.com>
2022-09-11 18:13:59 +0200

    % project

--- create_project.bat
+++ create_project.bat
@@ -10,7 +10,8 @@ cd CODE
 call npm install --global yarn
 call yarn install
 call yarn add -D baretest stylus svelte-preprocess
-call yarn add mysql2 senselogic-gist senselogic-eureka
+call yarn add -D tailwindcss autoprefixer postcss
+call yarn add daisyui mysql2 senselogic-gist senselogic-eureka
 call yarn audit
 pause
 call yarn run dev --open
