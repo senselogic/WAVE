792068db3d09871d92b3b3c66d758b9814d9f4af
senselogic <dev.senselogic@gmail.com>
2022-10-18 20:23:24 +0200

    % package

--- define_remote.bat
+++ define_remote.bat
@@ -1,8 +1,8 @@
 setx MYSQLDATABASE "wave"
 setx MYSQLHOST "localhost"
-setx MYSQLPASSWORD ""
 setx MYSQLPORT "3306"
 setx MYSQLUSER "root"
+setx MYSQLPASSWORD ""
 setx MYSQL_URL "mysql://root:@localhost:3306/wave"
 set
 pause
